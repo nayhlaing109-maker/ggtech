7f3493ae3ca82103b5ff988cff3fbf3432c55b46
Developer <developer@example.com>
2026-03-31 21:23:05 +0630

    Initial FCM v1 setup with GitHub Actions

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3oEgakHXj4NOos0QO61-HOuM165VOVYI",
+    authDomain: "pyapay-driver.firebaseapp.com",
+    projectId: "pyapay-driver",
+    storageBucket: "pyapay-driver.firebasestorage.app",
+    messagingSenderId: "719139320259",
+    appId: "1:719139320259:web:2e81dc869c0358135bcd76"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// နောက်ကွယ်မှာ message လက်ခံဖို့
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/favicon.ico' 
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
